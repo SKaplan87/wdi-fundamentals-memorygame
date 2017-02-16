8fe3ef4e3979276c0a2d31268412a836dba13dc8
Scott Kaplan <scottkaplan@Scotts-MBP.home>
2017-02-16 11:25:42 -0500

    Unit 10 additions

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,12 +1,29 @@
-console.log("JS file is connected to HTML! Woo!")
+/*console.log("JS file is connected to HTML! Woo!")
 var cardOne="Queen";
 var cardTwo="Queen";
 var cardThree="King";
 var cardFour="King";
 
-if (cardTwo == cardFour){
+
+
+if (cardOne == cardTwo){
 	alert ("You have found a match!")
 }
 else {
 	alert ("Sorry, try again")
+}*/
+
+var createCards = function()
+{
+	var numCards = 4;
+
+	for (i=0; i<numCards;i++)
+	{
+		var newCard = document.createElement('div');
+		newCard.className='card';
+		gameBoard.appendChild('newCard');
+	}
 }
+
+var gameBoard = document.getElementById('game-board');
+createCards();
