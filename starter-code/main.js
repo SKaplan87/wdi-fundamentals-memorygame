6568f2e0378c56388efe07f0a09902af2d668d2c
Scott Kaplan <scottkaplan@Scotts-MBP.home>
2017-02-24 07:45:02 -0500

    Unit 11 changes

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,28 +1,66 @@
-/*console.log("JS file is connected to HTML! Woo!")
-var cardOne="Queen";
-var cardTwo="Queen";
-var cardThree="King";
-var cardFour="King";
+var cards = ["queen", "queen","king","king"];
+var cardsInPlay = [];
+var wins=0;
+var losses=0;
 
 
 
-if (cardOne == cardTwo){
-	alert ("You have found a match!")
+var isMatch = function(cardsInPlay)
+{
+	if (cardsInPlay[0] == cardsInPlay[1])
+	{
+		alert ("You have found a match!")
+		wins++;
+	}
+	else 
+	{
+		alert ("Sorry, try again")
+		losses++;
+	}
+	alert(wins+" wins and "+losses+" losses");
+	reset();
 }
-else {
-	alert ("Sorry, try again")
-}*/
+
+var isTwoCards = function()
+{
+	cardsInPlay.push(this.getAttribute('data-card'));
+
+	if (this.getAttribute('data-card') == "king")
+	{
+		this.innerHTML = "<img src='king.png' alt = 'King'/>";
+	}
+	else 
+	{
+		this.innerHTML = "<img src='queen.png' alt = 'Queen'/>";
+	}
+
+
+	if (cardsInPlay.length == 2)
+	{
+		isMatch(cardsInPlay);
+	}
+}
 
 var createCards = function()
 {
-	var numCards = 4;
-
-	for (i=0; i<numCards;i++)
+	for (i=0; i<cards.length;i++)
 	{
 		var newCard = document.createElement('div');
 		newCard.className='card';
-		gameBoard.appendChild('newCard');
+		gameBoard.appendChild(newCard);
+		newCard.setAttribute('data-card', cards[i]);
+		newCard.addEventListener('click', isTwoCards);
 	}
+}
+
+var reset = function()
+{
+	for (var i=0; i<cards.length;i++)
+	{
+		document.getElementsByClassName('card')[i].innerHTML = "";
+
+	}
+	cardsInPlay = [];
 }
 
 var gameBoard = document.getElementById('game-board');
